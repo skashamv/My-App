c62b639a76c40bcdc0933fa4594f8245708faa21
Saksham Verma <saksham96@yahoo.com>
2023-03-26 22:46:49 +0530

    done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Box, Typography} from "@mui/material";
+
+const Footer = () => {
+  return (
+      <Box sx={{ mt: 'auto', p: '0.2rem', bgcolor: '#efefef', zIndex:1201 }}>
+        <Typography sx={{ textAlign: 'center', verticalAlign: 'middle' }}>
+          Made with love by Saksham Verma
+        </Typography>
+      </Box>
+  )
+}
+
+export default Footer
